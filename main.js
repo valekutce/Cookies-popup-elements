e809eef868ba6ae8d95b716f1157c3a724240318
valekutce <43315242+valekutce@users.noreply.github.com>
2022-02-22 10:15:51 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+let popupBlock = document.querySelector('.popup__block'),
+blockPopupClick = document.querySelector('.block__popup-click'),
+popupBlockClose = document.querySelector('.popup__block-close');
+
+blockPopupClick.addEventListener('click', function() {
+    popupBlock.classList.add('popup__block_active');
+});
+
+popupBlockClose.addEventListener('click', function() {
+    popupBlock.classList.remove('popup__block_active');
+});
